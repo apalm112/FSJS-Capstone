4e8947f82191597c2af6391a2a9ca75af7039d54
Adam <apalm112@gmail.com>
2018-11-16 17:24:43 -0500

    Removing .cache/ & /public/ folders from repo, they are not required there.  Those folders get created locally when from the command line via . '

--- gatsby-using-mongodb/.cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-
-exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/sofa_king/Team_Treehouse/Projects/Project12/fsjs-capstone-project/gatsby-using-mongodb/.cache/dev-404-page.js")),
-  "component---src-pages-index-js": preferDefault(require("/home/sofa_king/Team_Treehouse/Projects/Project12/fsjs-capstone-project/gatsby-using-mongodb/src/pages/index.js"))
-}
-
