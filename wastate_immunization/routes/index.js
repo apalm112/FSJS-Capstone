8ee581a650b43af688573cd3c1e1d40ae8a8e4d5
Adam <apalm112@gmail.com>
2019-03-07 13:12:55 -0500

    Finished the feature which on server startup checks the mLab DB for r School documents, if they aren't there then a get request is made to Socrata for the data.  Which is then stored in the DB.  The code is in the server.js file.

--- wastate_immunization/routes/index.js
+++ wastate_immunization/routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+// Function checks mLab DB if data is already there,
+// 		--if so, then do not fetch data
+	// let query = School.countDocuments();
+	// if (!query > 2594) {
+	// 	//		--if not, then do fetch data from socrata
+	//
+	// }
+	// 	.exec(function(error, schools) {
+	// 		res.json(schools);
+	// 	});
+
+
+
+
+
+
+
   res.render('index', { title: 'Express' });
 });
+
+
+
+
 
 module.exports = router;
--- wastate_immunization/server.js
+++ wastate_immunization/server.js
@@ -1,29 +1,35 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
-const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const http = require('https');
+const sassMiddleware = require('node-sass-middleware');
 const createError = require('http-errors');
+const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var schoolsRouter = require('./routes/schools');
+const indexRouter = require('./routes/index');
+const schoolsRouter = require('./routes/schools');
 
+const School = require('./database/models').School;
+
+const SOCRATA_API_KEY = process.env.SOCRATA_API_KEY;
 const MONGOLAB_URI  = process.env.MONGOLAB_URI;
 
-var app = express();
+const app = express();
 
 //	The port (3000) in the “proxy” line, which goes in the create-react-app's package.json file in the client folder, must match the port that your Express server is running on!
 let port = process.env.PORT || 3000;
 app.set('port', port);
 
+app.use(methodOverride('_method'));
 // morgan gives us http request logging output for the CLI
 app.use(logger('dev'));
 // app object registers middleware w/ use(), applies it to all routes.
-app.use(express.json());
+app.use(bodyParser.json());
 // express || body-parser middleware parses request to make it accessible to req.body
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(sassMiddleware({
 	src: path.join(__dirname, 'public'),
 	dest: path.join(__dirname, 'public'),
@@ -35,7 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Binds the routes to app object, mounts the routes to the express app specifiying '/' as the path.
 app.use('/', indexRouter);
-app.use('/schools', schoolsRouter);
+app.use('/', schoolsRouter);
 
 // Not sure about this line here, trying it out tho:
 app.use(require('cors')());
@@ -59,6 +65,45 @@ database.once('open', () => {
 	console.log('\n                \x1b[42m%s\x1b[0m', '-----------------Database Connection Successfully Opened------------------------');
 });
 /***********************************************************/
+
+
+/* On server start up immediately check mLab DB for documents,if not there then fetch Socratea data, save to mLab DB. ********/
+var socrata = 'https://data.wa.gov/resource/ndsp-2k9r.json?';
+var url = `${socrata}&$limit=3000&$$app_token=${SOCRATA_API_KEY}`;
+// Query checks mLab DB if data is already saved
+let query = School.countDocuments({ }, (err, count) => {
+	console.log('COUNT===========================',	count );
+	if (!count) {
+		//	--if not, then do fetch data from socrata
+		console.log('COUNT IS TRUE');
+		http.get(url, (res) => {
+			var body = '';
+			res.on('data',  (chunk) => {
+				body += chunk;
+			});
+			res.on('end', () => {
+				var data = JSON.parse(body);
+				console.log('# of schools: ', data[0]);
+				data.map( (eachSchool) => {
+					var school = new School(eachSchool);
+					school.save( (error) => {
+						if(error) return next(error);
+					});
+				});
+			}).on('error', (e) => {
+				console.log('Got an error: ', e);
+			});
+		});
+		console.log('Express, data got fetched');
+	} else {
+		// 	--if so, then do not fetch data
+		console.log('COUNT IS FALSE');
+		console.log(':::::::::::::::::::: DB is already populated ::::::::::::');
+		console.log('Express, DB data already populated in mLab.');
+	}
+});
+/************************************************************/
+
 
 /* Error Handling **********************************************/
 // catch 404 and forward to error handler
