a78ec1b5dbf02184d1ddaa3373fd98e223a8e408
Adam <apalm112@gmail.com>
2019-03-08 10:24:29 -0500

    Attempt to fix bug w/ the socrataView functions which populate the mLab DB. It was saving the Socrata data multiple times instead of just once. Should be fixed now.

--- wastate_immunization/public/javascripts/socrataView.js
+++ wastate_immunization/public/javascripts/socrataView.js
@@ -11,43 +11,43 @@
 
 	var socrataView = {};
 
-	socrataView.fetchData = function() {
+	socrataView.fetchAll = function() {
 		const socrata = 'https://data.wa.gov/resource/ndsp-2k9r.json?';
 		const url = `${socrata}&$limit=3000&$$app_token=${SOCRATA_API_KEY}`;
 
-		const fetchData = function () {
-			http.get(url, (res) => {
-				var body = '';
-				res.on('data',  (chunk) => {
-					body += chunk;
-				});
-				res.on('end', () => {
-					var data = JSON.parse(body);
-					console.log('# of schools: ', data[0]);
-					data.map( (eachSchool) => {
-						var school = new School(eachSchool);
-						school.save( (error) => {
-							if(error) return next(error);
-						});
+		http.get(url, (res) => {
+			var body = '';
+			res.on('data',  (chunk) => {
+				body += chunk;
+			});
+			res.on('end', () => {
+				var data = JSON.parse(body);
+				console.log('# of schools: ', data[0]);
+				data.map( (eachSchool) => {
+					var school = new School(eachSchool);
+					school.save( (error) => {
+						if(error) return next(error);
 					});
-				}).on('error', (e) => {
-					console.log('Got an error: ', e);
 				});
-			})};
+			}).on('error', (e) => {
+				console.log('Got an error: ', e);
+			});
+		})};
 
-		// Query checks mLab DB if data is already saved
+	socrataView.checkMLabDBForData = function() {
+	// Query checks mLab DB if data is already saved
 		let query = School.countDocuments({ }, (err, count) => {
 			console.log('COUNT===========================',	count );
-			//	--if not, then do fetch data from socrata
 			if (!count) {
 				console.log('COUNT IS TRUE  Express, data got fetched');
-				fetchData();
-				// 	--if so, then do not fetch data
+				//	--if not, then do fetch data from socrata
+				socrataView.fetchAll();
 			} else {
+				// 	--if so, then do not fetch data
 				console.log(':::::::::::::::::::: COUNT IS FALSE    DB is already populated ::::::::::::');
 			}
 		});
-	}
+	};
 
 	module.socrataView = socrataView;
 
--- wastate_immunization/server.js
+++ wastate_immunization/server.js
@@ -71,44 +71,45 @@ database.once('open', () => {
 /* Validate mLab Schools collection if it's already populated or not.************************/
 const socrataView = {};
 
-socrataView.fetchData = function() {
+socrataView.fetchAll = function() {
 	const socrata = 'https://data.wa.gov/resource/ndsp-2k9r.json?';
 	const url = `${socrata}&$limit=3000&$$app_token=${SOCRATA_API_KEY}`;
 
-	const fetchData = function () {
-		http.get(url, (res) => {
-			var body = '';
-			res.on('data',  (chunk) => {
-				body += chunk;
-			});
-			res.on('end', () => {
-				var data = JSON.parse(body);
-				console.log('# of schools: ', data[0]);
-				data.map( (eachSchool) => {
-					var school = new School(eachSchool);
-					school.save( (error) => {
-						if(error) return next(error);
-					});
+	http.get(url, (res) => {
+		var body = '';
+		res.on('data',  (chunk) => {
+			body += chunk;
+		});
+		res.on('end', () => {
+			var data = JSON.parse(body);
+			console.log('# of schools: ', data[0]);
+			data.map( (eachSchool) => {
+				var school = new School(eachSchool);
+				school.save( (error) => {
+					if(error) return next(error);
 				});
-			}).on('error', (e) => {
-				console.log('Got an error: ', e);
 			});
-		})};
+		}).on('error', (e) => {
+			console.log('Got an error: ', e);
+		});
+	})
+};	// end socrataView.fetchAll()
 
+socrataView.checkMLabDBForData = function () {
 	// Query checks mLab DB if data is already saved
 	let query = School.countDocuments({ }, (err, count) => {
 		console.log('COUNT===========================',	count );
 		//	--if not, then do fetch data from socrata
 		if (!count) {
 			console.log('COUNT IS TRUE  Express, data got fetched');
-			fetchData();
+			socrataView.fetchAll();
 			// 	--if so, then do not fetch data
 		} else {
 			console.log(':::::::::::::::::::: COUNT IS FALSE    DB is already populated ::::::::::::');
 		}
 	});
-}
-socrataView.fetchData();
+};
+socrataView.checkMLabDBForData();
 
 
 /* Error Handling ****************************************************************************/
