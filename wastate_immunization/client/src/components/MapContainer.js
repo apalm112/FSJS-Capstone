ddd26599ff20ed02f021752bbd29261d538531e3
Adam <apalm112@gmail.com>
2019-03-19 08:58:51 -0400

    Added a line of code that will close all open infoWindows when the map is clicked. The docs at 'https://github.com/googlemaps/v3-utility-library/' have all of the methods listed for the google maps, markers, marker clusterer & infowindows.

--- wastate_immunization/client/src/components/MapContainer.js
+++ wastate_immunization/client/src/components/MapContainer.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-// eslint-disable-next-line
 import Loading from './Loading';
-import MapV2 from './MapV2';
 import MarkerClusterer from '@google/markerclusterer';
 
 class MapContainer extends Component {
@@ -11,28 +9,13 @@ class MapContainer extends Component {
 	}
 
 	initMap = (map, gotData) => {
+		// TODO: map over this.props.schools to add Something fo marker label? Or not?
 		// Create an array of alphabetical characters used to label the markers.
 		var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 		// TODO: Edit the infoWindow so it renders each schools data
-		// probably have to modify the routes/ to return the
-		// required data.  Right now only coordinates get returned.
-/*		var contentString = gotData.map((school, idx) => {
-			// Works!  But needs to be a string.
-			var infoString = '<div id="content">'+
-      `<div id="siteNotice"><h2>Percent Complete for All Immuninzations: ${this.props.schools[0].allImms}%</h2></div>`+
-      `<h3 id="firstHeading" class="firstHeading">${this.props.schools[0].name}</h3>`+
-      '<div id="bodyContent">'+
-      `<h3>${this.props.schools[idx].address}</h3>`+
-      `<h3>${this.props.schools[idx].district}</h3>`+
-      '</div>'+
-      '</div>';
-			console.log('INFOSTIRNG HERE:::::::: ', infoString);
-			return infoString;
-		});
-		console.log('INFOSTIRNG HERE:::::::: ', typeof(contentString));*/
-/* eslint-disable */
-
+		// probably have to modify the routes/ to return the required data.
+		/* eslint-disable */
 		/*	school_district
 			school_name
 			school_year
@@ -55,16 +38,7 @@ class MapContainer extends Component {
 			percent_with_religious_exemption
 			percent_with_religious_membership_exemption*/
 
-	//   JSON.stringify(contentString)
-
-	/*		var schools = this.props.schools;
-			var gub = schools.map((school, idx) => {
-				// return position = { lng: school[0].lng, lat: school[0].lat }; <-----original & works
-				var position = { lng: school.lng, lat: school.lat };
-				// console.log('XXXXXXXXXXXXXXXXXXXXX', position);
-				return position;
-			});*/
-
+			//   JSON.stringify(contentString)
 	// TODO: Change the current schools.json to a changeable parameter that can be taken from the text value of a submit button click event,
 		var markers = gotData.map((school, idx) => {
 			// Add some markers to the map.
@@ -76,38 +50,45 @@ class MapContainer extends Component {
 				content:
 									'<div id="content">'+
 									`<div id="siteNotice"><h2>${this.props.schools[idx].specificRouteData}</h2></div>`+
-									`<h3 id="firstHeading" class="firstHeading">School:  ${this.props.schools[idx].name}</h3>`+
+									`<h3 id="firstHeading" class="firstHeading">${this.props.schools[idx].name}</h3>`+
 									'<div id="bodyContent">'+
 									`<h5>Grade Levels: ${this.props.schools[idx].levels}</h5>`+
-									`<h5>K-12 Enrollment: ${this.props.schools[idx].k12}</h5>`+
-									`<h5>${this.props.schools[idx].address}</h5>`+
-									`<h5>City: ${this.props.schools[idx].city}</h5>`+
-									`<h5>School District:  ${this.props.schools[idx].district}</h5>`+
+									`<h5>Enrollment: ${this.props.schools[idx].k12}</h5>`+
+									`<h6>${this.props.schools[idx].address}</h6>`+
+									`<h6>${this.props.schools[idx].city}</h6>`+
+									`<h6>${this.props.schools[idx].district}</h6>`+
 									'</div>'+
-									'</div>'
-								,
+									'</div>',
+				boxStyle: {	button: { background: 'darkgreyslate' } }
 			});
 			var infowindow = new window.google.maps.InfoWindow({ content: marker.content });
-			// console.log('INFOSTIRNG HERE:::::::: ', content);
+
 			window.google.maps.event.addListener(marker, 'click', function(event) {
-				// infowindow.setContent(content);
+				// infowindow.setOptions(opt_opts:	background: { 'darkgreyslate' }});
 				infowindow.open(map, marker);
 			});
+
+			// Event that closes the Info Window with a click on the map
+			google.maps.event.addListener(map, 'click', function() {
+				infowindow.close();
+			});
+
+
 			return marker;
 		});	// End of searchQuery.map()
 
 		new MarkerClusterer(map, markers, {imagePath: 'images/m'});
 	}	// End of initMap()
 
-	onScriptLoad() {
+	onScriptLoad(gotData) {
 		const map = new window.google.maps.Map(
 			document.getElementById('waMap'),
 			{	center: { lat: 47.3232, lng: -120.3232 },	zoom: 4	},
 			this.initMap);
 
-		const gotData = this.props.schools;
+		// const gotData = this.props.schools;
 
-		this.initMap(map, gotData);
+		this.initMap(map, (gotData=this.props.schools) );
 	}
 
 	componentDidMount() {
@@ -132,11 +113,16 @@ class MapContainer extends Component {
 		}
 	}
 
-	componentDidUpdate(prevProps) {
-		if (this.props.schools !== prevProps.schools) {
-			this.onScriptLoad();
+/*	componentDidUpdate(prevProps) {
+		if (this.props.searchQuery !== prevProps.searchQuery) {
+			this.props.handleSchoolQuery(this.props.searchQuery);
 		}
-	}
+	}*/
+/*	UNSAFE_componentWillReceiveProps(props) {
+		(this.props.searchQuery)
+		? this.props.handleSchoolQuery(this.props.searchQuery)
+		: this.props.handleSchoolQuery()
+	}*/
 
   render() {
 
@@ -155,11 +141,3 @@ class MapContainer extends Component {
 }
 
 export default MapContainer;
-
-/*	return (
-		<div>
-			{ (this.props.isLoading) ? <Loading />
-			: <MapV2 />
-			}
-		</div>
-		);*/
