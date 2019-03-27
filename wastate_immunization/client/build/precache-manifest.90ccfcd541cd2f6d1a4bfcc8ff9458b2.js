deb39b1d12cdd13605cdd26d996b71f79230b509
Adam <apalm112@gmail.com>
2019-03-27 13:10:42 -0400

    Updated the client .gitignore file, ran 'npm run build'.

---
+++ wastate_immunization/client/build/precache-manifest.90ccfcd541cd2f6d1a4bfcc8ff9458b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38f00e0cc0f44ac99b6c",
+    "url": "/static/js/main.83b30350.chunk.js"
+  },
+  {
+    "revision": "d48ed1250c43468af815",
+    "url": "/static/js/2.cde1e075.chunk.js"
+  },
+  {
+    "revision": "38f00e0cc0f44ac99b6c",
+    "url": "/static/css/main.5866c09e.chunk.css"
+  },
+  {
+    "revision": "a96b93c55511d8d091305be3b6207858",
+    "url": "/index.html"
+  }
+];
