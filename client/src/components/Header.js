9a934063a629210d8066c44bb86ba32739284bac
Adam <apalm112@gmail.com>
2019-03-28 17:05:49 -0400

    In the NavBar component changed the dropdown menus to buttons, added some CSS styles to them, added .scrollBy() to auto center on the map after a dropdown link is clicked on. Looks better now.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,8 @@ const Header = () => {
 			<p className="lead"> You can view school locations sorted by immunization rates, different types of immunizations & by exemption reasons.
 			</p>
 			<hr className="line"/>
-		  <p className="push-to-bottom">Select any category from the dropdown menus below to display the school data associated with it.</p>
+		  <p className="push-to-bottom">Each drop down menu selection displays the school data for that single category.
+			</p>
 		</div>
 	);
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,62 +2,60 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>
-			</button>
+	<nav className="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
+		<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
+		</button>
 
-			<div className="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul className="navbar-nav mr-auto">
-					<li className="nav-item dropdown">
-						<a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Schools</a>
-						<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-							<NavLink className="dropdown-item" to="/school/complete_for_all">100% Immunization Rates</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/school/reported_yes">Reported Yes</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/school/reported_no">Reported No</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/school/all">All Schools</NavLink>
-						</div>
-					</li>
-
-					<li className="nav-item dropdown">
-						<a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Exemption by Immunization Type
-						</a>
-						<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-							<NavLink className="dropdown-item" to="/immunization/hepatitis_b">Hepatitis B</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/immunization/measles">Measles</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/immunization/pertussis">Pertussis</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/immunization/polio">Polio</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/immunization/tetanus">Tetanus</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/immunization/varicella">Varicella</NavLink>
-						</div>
-					</li>
-
-					<li className="nav-item dropdown">
-						<a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Exemption by Reason
-						</a>
-						<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-							<NavLink className="dropdown-item" to="/reason/medical">Medical</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/reason/personal">Personal</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/reason/religous">Religous</NavLink>
-							<div className="dropdown-divider"></div>
-							<NavLink className="dropdown-item" to="/reason/any_exemption">Any Exemption</NavLink>
-						</div>
-					</li>
-				</ul>
+		<div className="collapse navbar-collapse" id="navbarSupportedContent">
+			<div className="btn-group">
+				<button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Schools
+				</button>
+				<div className="dropdown-menu">
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/school/complete_for_all">100% Immunization Rate</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/school/reported_yes">Reported Yes</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/school/reported_no">Reported No</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/school/all">All Schools</NavLink>
+				</div>
 			</div>
-		</nav>
+			<div className="btn-group">
+				<button type="button" className="btn btn-primary dropdown-toggle middle-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Exemption by Immunization Type
+				</button>
+				<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/immunization/hepatitis_b">Hepatitis B</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/immunization/measles">Measles</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/immunization/pertussis">Pertussis</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/immunization/polio">Polio</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/immunization/tetanus">Tetanus</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/immunization/varicella">Varicella</NavLink>
+				</div>
+			</div>
+			<div className="btn-group">
+				<button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Exemption By Reason
+				</button>
+				<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/reason/medical">Medical</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/reason/personal">Personal</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/reason/religous">Religous</NavLink>
+					<div className="dropdown-divider"></div>
+					<NavLink className="dropdown-item" data={window.scrollBy(0, 450)} to="/reason/any_exemption">Any Exemption</NavLink>
+				</div>
+			</div>
+		</div>
+	</nav>
 );
 
 export default NavBar;
