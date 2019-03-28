a754c86127f31630b766725c73caa9c38c5681f2
Adam <apalm112@gmail.com>
2019-03-28 10:57:41 -0400

    Added a function comment for this file.

--- wastate_immunization/client/src/modules/load-google-maps.js
+++ wastate_immunization/client/src/modules/load-google-maps.js
@@ -1,4 +1,8 @@
 const loadGoogleMaps = (callback) => {
+	/*	This function checks the index.html file for the Google Maps script tag
+			if it isn't there, then it adds one. Then executes the callback initMap(),
+			which is passed to it in the MapContainer component.
+	*/
 	const existingScript = document.getElementById('googleMaps');
 
 	if (!existingScript) {
